bc071c70f2bdbfde2c1a069cf2ec7ec138ec2027
Hongye <hongye.xiong@outlook.com>
2023-01-23 15:12:33 -0800

    master forth commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-        System.out.println("Hello 3333333!");
+        System.out.println("Hello master test");
     }
 }
