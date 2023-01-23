42ac027c5168b6fad734750bbe41285e9fff6c94
Hongye <hongye.xiong@outlook.com>
2023-01-23 15:06:38 -0800

    hot-fix first commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,6 +5,6 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("Hello world!");
         System.out.println("Hello world!");
-        System.out.println("Hello 22222!");
+        System.out.println("Hello 3333333!");
     }
 }
